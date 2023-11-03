37a235901bd21937c402cc13973541805460899f
Mate Eva <mateeva1022@gmail.com>
2023-11-03 09:05:19 +0100

    Cerated logic layer: Astronaut CRUD

---
+++ QCYDS9_HFT_2023241/QCYDS9_HFT_2023241.Logic/AstronautLogic.cs
@@ -0,0 +1,48 @@
+﻿using QCYDS9_HFT_2023241.Models;
+using QCYDS9_HFT_2023241.Repository;
+using System;
+using System.Linq;
+
+namespace QCYDS9_HFT_2023241.Logic
+{
+    public class AstronautLogic 
+    {
+        IRepository<Astronaut> repo;
+
+        public AstronautLogic(IRepository<Astronaut> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Astronaut item)
+        {
+            if (item.BornYear < 1900) throw new ArgumentException("Born year is not vaild.");
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Astronaut Read(int id)
+        {
+            var astronaut = this.repo.Read(id);
+            if (astronaut == null)
+            {
+                throw new ArgumentException("Astonaut nit exists.");
+            }
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Astronaut> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Astronaut item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
--- QCYDS9_HFT_2023241/QCYDS9_HFT_2023241.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace QCYDS9_HFT_2023241.Logic
-{
-    public class Class1
-    {
-    }
-}
